5978538b62766a62ac8dc2977453772a877c5697
arohovyi <andriirohovyi@gmail.com>
2021-01-29 11:13:20 +0200

    Fix issue with ORtools

--- backend/logistic/logistic_optimizer.py
+++ backend/logistic/logistic_optimizer.py
@@ -2,6 +2,7 @@ from typing import List, Tuple, Dict
 from haversine import haversine
 from itertools import combinations
 from cached_property import cached_property
+import ortools
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
@@ -42,24 +43,58 @@ class LogisticOptimizer(object):
             Example: ((0, 0), (45, 45)) : 2
 
         """
-        points_to_haversine = {}
-        for point_1, point_2 in combinations(self.total_locations, 2):
-            hv = haversine(point_1, point_2)
+        points_to_haversine = {(i, i): 0 for i in range(len(self.total_locations))}
+
+        for point_1, point_2 in combinations(enumerate(self.total_locations), 2):
+            hv = haversine(point_1[1], point_2[1])
             points_to_haversine.update({
-                (point_1, point_2): hv,
-                (point_2, point_1): hv
+                (point_1[0], point_2[0]): hv,
+                (point_2[0], point_1[0]): hv
             })
 
         return points_to_haversine
 
-    def weight_callback(self, from_index, to_index):
-        """Returns the distance between the two nodes."""
+    def weight_callback(self, from_index, to_index) -> float:
+        """
+        Returns the weight between the two nodes.
+
+        Parameters
+        ----------
+        from_index
+        to_index
+
+        Returns
+        -------
+        float
+            Weight of node (route)
+
+        """
         # Convert from routing variable Index to distance matrix NodeIndex.
         from_node = self.manager.IndexToNode(from_index)
         to_node = self.manager.IndexToNode(to_index)
-        return self.road_to_weight[from_node, to_node]
+        return self.road_to_weight[(from_node, to_node)]
 
-    def decode_solution(self, solution, routing):
+    def decode_solution(self,
+                        routing: ortools.constraint_solver.pywrapcp.RoutingModel,
+                        solution: ortools.constraint_solver.pywrapcp.Assignment
+                        ) -> List[List[Tuple[int, int]]]:
+        """
+        Decode ortools solution to REST format
+
+        Parameters
+        ----------
+        routing: ortools.constraint_solver.pywrapcp.RoutingModel
+            Routing that was using for solving Vehicle Routing Problem
+        solution: ortools.constraint_solver.pywrapcp.Assignment
+            Solution of problem
+
+        Returns
+        -------
+        List[List[Tuple[int, int]]]
+            List of routes that build for deliverymen
+            Example: [[(0, 0), (-84, -15), (-36, 107), (-71, -4), (23, 55)]]
+
+        """
         routes = []
         for vehicle_id in range(self.amount_of_delivery_man):
             index = routing.Start(vehicle_id)
@@ -73,14 +108,14 @@ class LogisticOptimizer(object):
 
         return routes
 
-    def solve_delivery_problem(self) -> List[List[Tuple[float, float]]]:
+    def solve_delivery_problem(self) -> List[List[Tuple[int, int]]]:
         """
         Method for solving delivery problem.
         Depending from hardness of request, we choose different solution to solve a problem.
 
         Returns
         -------
-        List[List[Tuple[float, float]]]
+        List[List[Tuple[int, int]]]
             List of sorted sequence of delivery points for each delivery man.
             Every points subset starts with store location
 
@@ -113,5 +148,6 @@ class LogisticOptimizer(object):
         # Solve the problem.
         solution = routing.SolveWithParameters(search_parameters)
 
-        return self.decode_solution(solution, routing)
+        return self.decode_solution(solution=solution,
+                                    routing=routing)
 
--- backend/logistic/metrics.py
+++ backend/logistic/metrics.py
@@ -1,12 +1,28 @@
 from haversine import haversine
-from typing import List
+from typing import List, Tuple
 
 
-def calculate_routes_distance(routes) -> List[float]:
+def calculate_routes_distance(routes: List[List[Tuple[int, int]]]) -> List[float]:
+    """
+    Function for calculating vector of distances for routes to measure efficiency of solution
+
+    Parameters
+    ----------
+    routes: List[List[Tuple[int, int]]]
+        List of routes that build for deliverymen
+        Example: [[(0, 0), (-84, -15), (-36, 107), (-71, -4), (23, 55)]]
+    Returns
+    -------
+    List[float]
+        List of distances
+        Example: [34079.48355817763] (for the case up)
+
+    """
     distances = []
     distance = 0
     for route in routes:
         for i, point in enumerate(route[:-1]):
             distance += haversine(point, route[i + 1])
         distances.append(distance)
+
     return distances
